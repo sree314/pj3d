dd5c70a14a4f2854e2b8696104ef1064de26afe9
sree <sree@buckyball>
2022-12-24 14:02:25 -0500

    Add scripts

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.0.1',
     install_requires=[],
     packages=find_packages(),
-    scripts=['bin/plater3d']
+    scripts=['bin/plater3d', 'bin/parse_cli.py', 'bin/platepacker', 'bin/printplate', 'bin/vispackings', 'bin/diff_cura_settings.py']
 )
